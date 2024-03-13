7e31b831b308e6b191bf7c4dea6f185dcbff7f27
Velan <103123881+Velan483@users.noreply.github.com>
2024-03-13 15:33:17 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,5 @@
 class Sample{
   public static void main(String args[]){
-    System.out.println("Hello world");
-    String s=Hai;
-    System.out.print("herklkdsfjljdsdsfdsfdfdsfdsfdsdf");
+    
   }
 }
