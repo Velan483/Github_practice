7f1e16341b22c86375495f25c4ed9fa629feaaac
Velan <velan@gmail.com>
2024-03-13 15:29:57 +0530

    sdkfjoiej

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ class Sample{
   public static void main(String args[]){
     System.out.println("Hello world");
     String s=Hai;
-    System.out.print("herkl");
+    System.out.print("herklkdsfjljdsdsfdsfdfdsfdsfdsdf");
   }
 }
